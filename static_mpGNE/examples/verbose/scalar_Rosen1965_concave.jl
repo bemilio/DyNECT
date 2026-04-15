cdfa994350332556a3228f3afa270634933604c7
StephanieMattaB <stephanie.mattab@gmail.com>
2026-04-15 12:42:53 +0200

    fix: Rosen costs match paper exactly

--- static_mpGNE/examples/verbose/scalar_Rosen1965_concave.jl
+++ static_mpGNE/examples/verbose/scalar_Rosen1965_concave.jl
@@ -11,14 +11,13 @@ game = GameBuilder(N=2)
 @player game 2 n=1
 
 @cost game 1  0.5*x1^2 - x1*x2
-@cost game 2  0.5*x2^2 + x1*x2
+@cost game 2  x2^2 + x1*x2
 
 @constraint game  -x1 <= 0
 @constraint game  -x2 <= 0
 @constraint game  x1 + x2 >= 1
 
 ## Display
-println()
 #validate_game(game)
 #show_operator(game)
 #show_shared_constraints(game)
@@ -28,11 +27,28 @@ println()
 #show_theta_set(game)
 
 ## Build and solve
-println()
 mpvi = build_mpvi(game)
 show_mpvi(mpvi)
 
-println()
 sol = solve_gne(mpvi)
 show_solution(sol, mpvi)
-show_pwa_map(sol)
+show_pwa_map(sol) #testing linear map 
+
+for (i, cr) in enumerate(sol.CRs)
+    th = cr.th  
+    x = evaluate_gne(sol, (th - sol.translation) ./ sol.scaling)  
+    x = DyNECT.evaluatePWA(sol, th)
+    if x !== nothing
+        s = x[1] + x[2]
+        println("CR $i: θ=$(round.(cr.th, digits=3)), x*=$(round.(x, digits=3)), x1+x2=$(round(s, digits=3))")
+    else
+        println("CR $i: θ=$(round.(cr.th, digits=3)) → infeasible")
+    end
+end
+
+println("\n=== CR boundaries in original θ space ===")
+for (i, cr) in enumerate(sol.CRs)
+    println("\n--- CR $i ---")
+    @show cr.Ath
+    @show cr.bth
+end
